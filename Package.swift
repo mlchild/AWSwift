f2e6a3b892fb71f82cd8320e52bd0690e3844865
Max Child <mlchild@gmail.com>
2017-04-02 14:00:15 -0700

    Trying to fix crypto dependency version issue

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "AWSwift",
     dependencies: [
-        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
+        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", "0.6.8")
     ]
 )
